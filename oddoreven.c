e9df13f4f0db971b0866caa3e83dc741ae37846b
manobharthi73 <38652770+manobharthi73@users.noreply.github.com>
2018-04-24 09:36:39 +0000

    Create oddoreven.c

---
+++ oddoreven.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+  long int Input;
+  printf("Input:");
+  scanf("%ld",&Input);
+  if(Input==0 && Input<100000)
+  {
+    if(Input%2==0 || Input==0)
+    {
+      printf("Output:\nEven");
+    }
+    else 
+    {
+      printf("Output:\nOdd");
+    }
+  }
+    else
+    {
+      printf("Invalid Input");
+    }
+return 0;
+}
